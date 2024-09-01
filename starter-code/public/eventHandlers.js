581d3a03844f66dfa187d5e2e79cf6c59310fd66
akakaaaaa <akakitsipuria1@gmail.com>
2024-09-01 22:16:59 +0400

    first commit

--- starter-code/public/eventHandlers.js
+++ starter-code/public/eventHandlers.js
@@ -1,13 +1,36 @@
 $(document).ready(function () {
-    $(".header-switch").click(function (e) { 
+
+    $("#input-submit").click(function (e) { 
         e.preventDefault();
-        $("body").toggleClass("dark-body");
-        if(!($("body").hasClass("dark-body"))) {
-            $(".switch-name").html("Dark");
-            // $(".search-bar-img").attr("src ", "../public/assets/icon-moon.svg");
-        } else {
-            $(".switch-name").html("Light");
-            // $(".search-bar-img").attr("src ", "../public/assets/icon-sun.svg");
+        if($("#input-text").html() === "") {
+            // $("#input-submit").css("opacity", "50%");
         }
     });
+
+    let theme = localStorage.getItem("theme") || "light-body";
+
+    if(theme === "dark-body"){
+        $("body").addClass("dark-body");
+        $(".switch-name").html("Light");
+    } else {
+        $("body").removeClass("dark-body");
+        $(".switch-name").html("Dark");
+    }
+
+    $(".header-switch").click(function (e) { 
+        e.preventDefault();
+
+        $("body").toggleClass("dark-body");
+
+        if($("body").hasClass("dark-body")) {
+            theme = "dark-body";
+            $(".switch-name").html("Light");
+        } else {
+            theme = "light-body";
+            $(".switch-name").html("Dark");
+        }
+
+        localStorage.setItem("theme", theme);
+    });
+
 });
